e3bbfa6d153ae418593dea7805b5c00ea5b44efd
Joelson Nascimento <eu_joelson@hotmail.com>
2020-07-04 13:46:48 -0300

    Projeto criado

---
+++ src/testeCurso/TesteCurso.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package testeCurso;
+
+/**
+ * @author Eu_Jo
+ *
+ */
+public class TesteCurso {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("teste joelson");
+	}
+
+}
